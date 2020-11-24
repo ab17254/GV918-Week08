845a6ed9a26541624da292e2210ecc4ca216695c
Aki Matsuo <matsuoakitaka@gmail.com>
2020-11-24 11:39:19 +0000

    Add files via upload

---
+++ parse_tweets_json.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import json
+import csv
+import shutil
+import bz2
+from datetime import datetime
+import re
+import os
+
+
+# os.chdir(path)
+
+### rename the current file to another name 
+shutil.move('tweets.json', 'tweets_temp.json')
+
+### process tweets and save in a csv file
+
+csv_file = open('processed_tweets.csv', mode = 'a')
+with open('tweets_temp.json', 'rb') as f:
+    for line in f: 
+        tweet = json.loads(line) 
+        output = [tweet['id_str'], 
+                  tweet['text'],
+                  tweet['created_at'],
+                  tweet['user']['id_str'],
+                  tweet['user']['screen_name'],
+                  tweet['lang']]
+        c_writer = csv.writer(csv_file, delimiter=',', quotechar='"', 
+                   quoting=csv.QUOTE_MINIMAL)
+        c_writer.writerow(output)
+csv_file.close()
+
+
+### compress the source tweet file in bzip2 format
+
+#### decide file name
+
+source_file = 'tweets_temp.json'
+reg = re.compile('\W+')
+outfile = "tweets_" + reg.sub('-', str(datetime.now())[:19]) + '.json.bz2'
+
+#### compression
+
+comp = bz2.compress(open(source_file, 'rb').read(), compresslevel = 9)
+with open(outfile, 'wb') as fh:
+    fh.write(comp)
